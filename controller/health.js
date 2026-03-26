262af6f6909c73d46a954361ab66e90a961399fe
Ayush-gupta <136897914+AGJI-55@users.noreply.github.com>
2026-03-26 14:33:02 +0530

    Update email transport configuration in health.js

--- controller/health.js
+++ controller/health.js
@@ -14,6 +14,9 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+   host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
